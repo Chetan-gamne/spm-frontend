57aafe63513a8c0e0ef46b4660c40213fcd302c7
chetan <chetangamne12@gmail.com>
2023-02-06 15:42:24 +0530

    dashboard added and header added & footer added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,12 +9,27 @@ import createEmotionCache from "../config/createEmotionCache";
 import { ApolloProvider } from "@apollo/client";
 
 import client from "../services/apollo-client";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 const clientSideEmotionCache = createEmotionCache();
 
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
+
+const sections = [
+  { title: "Technology", url: "#" },
+  { title: "Design", url: "#" },
+  { title: "Culture", url: "#" },
+  { title: "Business", url: "#" },
+  { title: "Politics", url: "#" },
+  { title: "Opinion", url: "#" },
+  { title: "Science", url: "#" },
+  { title: "Health", url: "#" },
+  { title: "Style", url: "#" },
+  { title: "Travel", url: "#" },
+];
 
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
@@ -26,7 +41,9 @@ export default function MyApp(props: MyAppProps) {
         </Head>
         <ThemeProvider theme={theme}>
           <CssBaseline />
+          <Header sections={sections} title="SPM" />
           <Component {...pageProps} />
+          <Footer />
         </ThemeProvider>
       </CacheProvider>
     </ApolloProvider>
